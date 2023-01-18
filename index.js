248668ded178d9248ea1597bf1c6e6fe534f856a
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-18 04:17:11 -0300

    primera version

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+/* Información inicial de CV */
+
 const correo = document.getElementById('correo');
 const nombre = document.getElementById('nombre');
 const foto = document.getElementById('foto');
 const telefono = document.getElementById('telefono');
 
 let paginaActual = 1;
+
+/** Petición a la API */
 
 const generarUsuario = async () => {
     const url = `https://randomuser.me/api/?page=${paginaActual}&results=10&seed=abc`;
@@ -12,16 +16,43 @@ const generarUsuario = async () => {
     const { results } = await response.json()
     const datos = results[0];
 
-    document.getElementById('foto').src = datos.picture.medium
-    document.getElementById('nombre').innerHTML = datos.name.title + " " + datos.name.first + " " + datos.name.last
-    document.getElementById('telefono').innerHTML = datos.phone
-    document.getElementById('correo').innerHTML = datos.email
+    console.log(datos);
+    document.getElementById('foto').src = datos.picture.large;
+    document.getElementById('nombre').innerHTML = datos.name.title + " " + datos.name.first + " " + datos.name.last;
+    document.getElementById('telefono').innerHTML = datos.phone;
+    document.getElementById('correo').innerHTML = datos.email;
+    document.getElementById('age').innerHTML = datos.dob.age;
+    document.getElementById('location-city').innerHTML = datos.location.city;
+    document.getElementById('location-country').innerHTML = datos.location.country;
+    document.getElementById('location-state').innerHTML = datos.location.state;
+    document.getElementById('location-postcode').innerHTML = datos.location.postcode;
+    document.getElementById('location-street-name').innerHTML = datos.location.street.name;
+    document.getElementById('location-street-number').innerHTML = datos.location.street.number;
 }
-document.addEventListener('DOMContentLoaded', generarUsuario);
 
-//Funcionalidad de botones
+/* Dibujar en el dom */
+document.addEventListener('DOMContentLoaded', generarUsuario());
+
+/* Desplegar más información */
+
+const botonDesplegar = document.getElementById('botonDesplegar');
+const contenidoDesplegable = document.getElementById('contenidoDesplegable');
+
+botonDesplegar.addEventListener('click', () => {
+    if (contenidoDesplegable.style.display === 'none') {
+        contenidoDesplegable.style.display = 'block';
+        botonDesplegar.textContent = 'Ocultar información'
+    } else {
+        contenidoDesplegable.style.display = 'none';
+        botonDesplegar.textContent = '...'
+    }
+})
+
+/* Funcionalidad de botones */
+
 document.getElementById('boton-siguiente').addEventListener('click', () => {
     paginaActual += 1;
+
     generarUsuario();
 });
 document.getElementById('boton-anterior').addEventListener('click', () => {
@@ -30,3 +61,4 @@ document.getElementById('boton-anterior').addEventListener('click', () => {
     }
     generarUsuario();
 });
+
